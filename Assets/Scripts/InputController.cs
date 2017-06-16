6f1fb4be22bf43006b6e4a94e58a8416a96b822c
RNPGhost <samuelfhutton@gmail.com>
2017-06-16 21:24:02 +0100

    add basic player movement

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+  [SerializeField]
+  private TargetBasedMovementController playerMovement;
+  [SerializeField]
+  private Rigidbody playerRigidbody;
+  
+	private void Update () {
+    float horizontalInput = Input.GetAxisRaw("Horizontal");
+    if (horizontalInput != 0) {
+      playerMovement.SetTargetPosition(new Vector3(5 * horizontalInput, playerRigidbody.position.y, playerRigidbody.position.z));
+    }
+  }
+}
---
+++ Assets/Scripts/TargetBasedMovementController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBasedMovementController : MonoBehaviour {
+
+  [SerializeField]
+  private float speed;
+
+  private Rigidbody playerRigidbody;
+  private Vector3 targetPosition;
+  
+  public void SetTargetPosition(Vector3 newTargetPosition) {
+    targetPosition = newTargetPosition;
+  }
+
+  private void Start() {
+    playerRigidbody = GetComponent<Rigidbody>();
+    targetPosition = playerRigidbody.position;
+  }
+
+  private void FixedUpdate () {
+    MoveTowardsTargetPosition();
+	}
+
+  private void MoveTowardsTargetPosition() {
+    // Check proximity to target to prevent 0 rounding errors
+    float delta = 0.1f;
+    if (Vector3.Distance(targetPosition, playerRigidbody.position) < delta) {
+      playerRigidbody.position = targetPosition;
+      playerRigidbody.velocity = Vector3.zero;
+    } else {
+      playerRigidbody.velocity = speed * (targetPosition - playerRigidbody.position).normalized;
+    }
+  }
+}
