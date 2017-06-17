8fde48e49133dcb5fa2aa3006113cccc59b0dda3
RNPGhost <samuelfhutton@gmail.com>
2017-06-17 17:27:23 +0100

    destroy particles when they're done

---
+++ Assets/Scripts/DestroyParticlesWhenDone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticlesWhenDone : MonoBehaviour {
+
+  private ParticleSystem particleSystem;
+
+  private void Update() {
+    if (particleSystem) {
+      if (!particleSystem.IsAlive()) {
+        Destroy(gameObject);
+      }
+    }
+  }
+
+  private void Start() {
+    particleSystem = GetComponent<ParticleSystem>();
+  }
+}
