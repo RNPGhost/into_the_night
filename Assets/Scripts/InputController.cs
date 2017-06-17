4fd733c4bb0d3433588ed270e94311fbdfe8199c
RNPGhost <samuelfhutton@gmail.com>
2017-06-17 11:52:19 +0100

    add touch input controls

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -8,11 +8,40 @@ public class InputController : MonoBehaviour {
   private TargetBasedMovementController playerMovement;
   [SerializeField]
   private Rigidbody playerRigidbody;
-  
-	private void Update () {
-    float horizontalInput = Input.GetAxisRaw("Horizontal");
-    if (horizontalInput != 0) {
-      playerMovement.SetTargetPosition(new Vector3(5 * horizontalInput, playerRigidbody.position.y, playerRigidbody.position.z));
+  [SerializeField]
+  private Camera mainCamera;
+
+  private void Update () {
+
+    List<Touch> touches = new List<Touch>(Input.touches);
+
+    // Add pc support
+    if (Input.GetButton("Fire1")) {
+      touches.Add(new Touch() {
+        position = new Vector2(Input.mousePosition.x, Input.mousePosition.y)
+      });
     }
+    
+    if (touches.Count > 0) {
+      // Only control player position with the closest touch
+      Touch closestTouchToPlayer = GetClosestTouch(touches, playerRigidbody.position);
+      Vector3 closestTouchToPlayerWorldPosition = mainCamera.ScreenToWorldPoint(closestTouchToPlayer.position);
+      playerMovement.SetTargetPosition(new Vector3(closestTouchToPlayerWorldPosition.x, playerRigidbody.position.y, playerRigidbody.position.z));
+    }
+  }
+
+  private Touch GetClosestTouch(List<Touch> touches, Vector3 position) {
+    // Returns a new Touch if the touches array is empty
+    Touch closestTouch = new Touch();
+    float bestDistance = -1;
+    foreach (Touch touch in touches) {
+      Vector3 touchPosition = mainCamera.ScreenToWorldPoint(touch.position);
+      float distance = Vector3.Distance(position, touchPosition);
+      if (bestDistance == -1 || distance < bestDistance) {
+        bestDistance = distance;
+        closestTouch = touch;
+      }
+    }
+    return closestTouch;
   }
 }
