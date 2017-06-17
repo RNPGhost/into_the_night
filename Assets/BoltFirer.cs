4a449bb8e0096d83263ff0acac7d8e62e9ce79b5
RNPGhost <samuelfhutton@gmail.com>
2017-06-17 10:17:41 +0100

    allow player to spawn bolts

---
+++ Assets/BoltFirer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltFirer : MonoBehaviour {
+
+  [SerializeField]
+  private GameObject boltPrefab;
+  [SerializeField]
+  private Transform boltSpawn;
+  [SerializeField]
+  private bool fireOnStart;
+  [SerializeField]
+  private float boltCooldown;
+
+  private float boltTimer;
+  private bool fire;
+
+  public void StartFiring() {
+    fire = true;
+  }
+
+  public void StopFiring() {
+    fire = false;
+  }
+
+  private void Start() {
+    boltTimer = 0;
+    fire = fireOnStart;
+  }
+
+  private void Update () {
+    boltTimer -= Time.deltaTime;
+    Debug.Log(fire);
+    if (fire && (boltTimer < 0)) {
+      Instantiate(boltPrefab, boltSpawn.position, boltSpawn.rotation);
+      boltTimer = boltCooldown;
+    }
+	}
+}
