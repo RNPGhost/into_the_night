e8761a61ca5d060745989e69e5704ecfcf2250ee
RNPGhost <samuelfhutton@gmail.com>
2017-06-17 09:56:11 +0100

    add bolt with visuals and movement

---
+++ Assets/Scripts/SingleDirectionMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleDirectionMover : MonoBehaviour {
+
+  [SerializeField]
+  private Vector3 defaultVelocity;
+
+  private Rigidbody rigidbody;
+
+  private void Start() {
+    rigidbody = GetComponent<Rigidbody>();
+    SetVelocity(defaultVelocity);
+  }
+
+  void SetVelocity (Vector3 velocity) {
+    rigidbody.velocity = velocity;
+    rigidbody.rotation = Quaternion.LookRotation(velocity);
+	}
+}
