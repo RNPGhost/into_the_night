f8350f0f2484ba67db8364cf2414858d825dd51c
RNPGhost <samuelfhutton@gmail.com>
2017-06-18 19:40:41 +0100

    add ability framework

---
+++ Assets/Scripts/AbilityController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityController : MonoBehaviour {
+
+  [SerializeField]
+  private Ability currentAbility;
+
+	public void ActivateAbility() {
+    currentAbility.Activate();
+  }
+}
---
+++ Assets/Scripts/AbilityWhirlwind.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityWhirlwind : Ability {
+
+  [SerializeField]
+  private float abilityCooldown;
+  [SerializeField]
+  private float abilityDuration;
+
+  private bool activated;
+  private float cooldownTimer;
+
+  private void Start() {
+    activated = false;
+  }
+
+  override public void Activate() {
+    if (CanActivate()) {
+      cooldownTimer = abilityCooldown;
+      activated = true;
+    }
+  }
+
+  override public float GetCooldown() {
+    return activated ? cooldownTimer : -1;
+  }
+
+  override public bool CanActivate() {
+    return !activated;
+  }
+
+  private void Update() {
+    cooldownTimer -= Time.deltaTime;
+    if (activated & (cooldownTimer < 0)) {
+      activated = false;
+    }
+
+  }
+}
